5b4d13f81143dd67eac54e4df7afce0716479092
Natali-1991 <natawa.bodnar@mail.ru>
2024-05-09 11:49:33 +0100

    Update 3.ts

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,6 +1,5 @@
 export {};
-function merge<T>(objA: T, objB: T): T | undefined {
-  return Object.assign(objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
-
 // У вас є функція merge, яка поєднує два об'єкти. Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
